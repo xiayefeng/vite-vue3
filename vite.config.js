98ae32e93dca7ecbcda2b5a8d2267a14fddb485a
xiayefeng <xiayu_12@yeah.net>
2023-08-06 01:41:42 +0800

    add gzip

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import eslint from 'vite-plugin-eslint'
 import path from 'node:path'
+import viteCompression from 'vite-plugin-compression'
 
 function resolve (dir) {
   return path.join(__dirname, dir)
@@ -18,9 +19,41 @@ export default defineConfig({
       emitFile: false,
       file: "stats.html", //分析图生成的文件名
       open: true //如果存在本地服务端口，将在打包后自动展示
-    })
+    }),
+    viteCompression({
+      verbose: true,
+      disable: false, // 不禁⽤压缩
+      deleteOriginFile: false, // 压缩后是否删除原⽂件
+      threshold: 10240, // 压缩前最⼩⽂件⼤⼩
+      algorithm: 'gzip', // 压缩算法
+      ext: '.gz', // ⽂件类型
+    }),
+    
   ],
   resolve: {
     '@': resolve('src')
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendor': ['vue', 'vue-router', 'pinia'],
+          'echarts': ['echarts'],
+          'lodash': ['lodash'],
+        },
+      },
+    },
+    terserOptions: {
+      compress: {
+        // warnings: false,
+        drop_console: true, // 打包时删除console
+        drop_debugger: true, // 打包时删除 debugger
+        pure_funcs: ['console.log'],
+      },
+      output: {
+        // 去掉注释内容
+        comments: true,
+      },
+    },
   }
 })
